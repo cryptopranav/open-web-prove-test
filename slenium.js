51548c8cf41c4efc8085b6a857e7a61057cef021
cryptopranav <pranavdileepcy@gmail.com>
2025-06-30 13:08:28 +0530

    Update slenium.js

--- slenium.js
+++ slenium.js
@@ -3,7 +3,7 @@ import { Builder, By } from 'selenium-webdriver'
 
 async function runTestOnSafari() {
     let currentfuncalltime = Date.now();
-    let maxsecondsfunctioncall = 360 * 2;
+    let maxsecondsfunctioncall = 360 * 4;
     const driver = await new Builder().forBrowser('safari').build();
 
     try {
